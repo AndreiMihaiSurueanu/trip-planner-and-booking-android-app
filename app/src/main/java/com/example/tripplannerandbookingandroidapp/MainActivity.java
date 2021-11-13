e6f73304a3057df0d0b7106618668650eab09a09
andrei <git1@email.com>
2021-11-13 07:38:45 +0100

    Display Data in Recents Recycler
    * Display Data in the Recents Recycler View inside the Main Activity

--- app/src/main/java/com/example/tripplannerandbookingandroidapp/MainActivity.java
+++ app/src/main/java/com/example/tripplannerandbookingandroidapp/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.tripplannerandbookingandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.tripplannerandbookingandroidapp.adapter.RecentsAdapter;
+import com.example.tripplannerandbookingandroidapp.model.RecentsData;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.RecursiveAction;
+
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recentRecycler;
+    RecentsAdapter recentsAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<RecentsData> recentsDataList = new ArrayList<>();
+        recentsDataList.add(new RecentsData("AM Lake","India","From $200",R.drawable.recentimage1));
+        recentsDataList.add(new RecentsData("Nilgiri Hills","India","From $300",R.drawable.recentimage2));
+        recentsDataList.add(new RecentsData("AM Lake","India","From $200",R.drawable.recentimage1));
+        recentsDataList.add(new RecentsData("Nilgiri Hills","India","From $300",R.drawable.recentimage2));
+        recentsDataList.add(new RecentsData("AM Lake","India","From $200",R.drawable.recentimage1));
+        recentsDataList.add(new RecentsData("Nilgiri Hills","India","From $300",R.drawable.recentimage2));
+
+        setRecentRecycler(recentsDataList);
+
+    }
+
+    private  void setRecentRecycler(List<RecentsData> recentsDataList){
+
+        recentRecycler = findViewById(R.id.recent_recycler);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+        recentRecycler.setLayoutManager(layoutManager);
+        recentsAdapter = new RecentsAdapter(this, recentsDataList);
+        recentRecycler.setAdapter(recentsAdapter);
+
     }
 }
